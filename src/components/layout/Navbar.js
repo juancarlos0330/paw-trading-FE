74acd7347fcc0935880fb1db397e3ec84eebe47e
juancarlos0330 <juancarlos.king0330@gmail.com>
2024-12-12 20:49:22 +0900

    - Update the navbar

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 // import icons
@@ -15,40 +16,73 @@ import { faSun } from "@fortawesome/free-solid-svg-icons";
 import "./Navbar.scss";
 
 const Navbar = () => {
+  const location = useLocation();
+  const urlPath = location.pathname;
+
   return (
     <div className="navbar-container">
-      <a href="/overview/chain" className="navbar-item">
+      <a
+        href="/overview/chain"
+        className={`navbar-item ${
+          urlPath === "/overview/chain" ? "active" : ""
+        }`}
+      >
         <FontAwesomeIcon icon={faHouse} style={{ fontSize: "24px" }} />
         <span className="navbar-text">Chain</span>
       </a>
-      <a href="/overview/project" className="navbar-item">
+      <a
+        href="/overview/project"
+        className={`navbar-item ${
+          urlPath === "/overview/project" ? "active" : ""
+        }`}
+      >
         <FontAwesomeIcon icon={faCircleInfo} style={{ fontSize: "24px" }} />
         <span className="navbar-text">Project</span>
       </a>
-      <a href="/dashboard/analytics" className="navbar-item">
+      <a
+        href="/dashboard/analytics"
+        className={`navbar-item ${
+          urlPath === "/dashboard/analytics" ? "active" : ""
+        }`}
+      >
         <FontAwesomeIcon icon={faChartSimple} style={{ fontSize: "24px" }} />
         <span className="navbar-text">Dashboard</span>
       </a>
-      <a href="#" className="navbar-item">
+      <a
+        href="#"
+        className={`navbar-item ${urlPath === "/sss" ? "active" : ""}`}
+      >
         <FontAwesomeIcon icon={faComments} style={{ fontSize: "24px" }} />
         <span className="navbar-text">Dashboard</span>
       </a>
-      <a href="#" className="navbar-item">
+      <a
+        href="#"
+        className={`navbar-item ${urlPath === "/sss" ? "active" : ""}`}
+      >
         <FontAwesomeIcon icon={faSun} style={{ fontSize: "24px" }} />
         <span className="navbar-text">Dashboard</span>
       </a>
-      <a href="#" className="navbar-item">
+      <a
+        href="#"
+        className={`navbar-item ${urlPath === "/sss" ? "active" : ""}`}
+      >
         <FontAwesomeIcon
           icon={faSquarePollVertical}
           style={{ fontSize: "24px" }}
         />
         <span className="navbar-text">Dashboard</span>
       </a>
-      <a href="#" className="navbar-item">
+      <a
+        href="#"
+        className={`navbar-item ${urlPath === "/sss" ? "active" : ""}`}
+      >
         <FontAwesomeIcon icon={faChartColumn} style={{ fontSize: "24px" }} />
         <span className="navbar-text">Dashboard</span>
       </a>
-      <a href="#" className="navbar-item">
+      <a
+        href="#"
+        className={`navbar-item ${urlPath === "/sss" ? "active" : ""}`}
+      >
         <FontAwesomeIcon icon={faAddressBook} style={{ fontSize: "24px" }} />
         <span className="navbar-text">Dashboard</span>
       </a>
