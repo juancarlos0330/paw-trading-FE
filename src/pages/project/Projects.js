ffe425fdc1dc4cb4e743916729a76084e7665c5c
juancarlos0330 <juancarlos.king0330@gmail.com>
2024-12-17 03:10:03 +0900

    -update

--- src/pages/project/Projects.js
+++ src/pages/project/Projects.js
@@ -5,6 +5,9 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faBookmark,
   faMagnifyingGlass,
+  faChevronDown,
+  faLink,
+  faBorderAll,
 } from "@fortawesome/free-solid-svg-icons";
 
 // import style
@@ -120,6 +123,60 @@ const data = [
       "https://cdn.prod.website-files.com/65035c85edfbbecd98169f8d/667f65cf69cd32468cfd6c38_477B2EF5-8039-4077-B176-A9EDC2403FDD-p-500.png",
     types: ["Defi Tool"],
   },
+  {
+    id: 13,
+    name: "Ethereum",
+    desciption:
+      "Balancer is a community-owned and -governed decentralized exchange",
+    imgUrl:
+      "https://cdn.prod.website-files.com/65035c85edfbbecd98169f8d/6509e2826e7f9dcddbf8354f_ethereum-eth-logo-p-500.png",
+    types: ["DEX", "DEX Aggregator", "Defi Tool"],
+  },
+  {
+    id: 14,
+    name: "Binance",
+    desciption:
+      "Balancer is a community-owned and -governed decentralized exchange",
+    imgUrl:
+      "https://cdn.prod.website-files.com/65035c85edfbbecd98169f8d/667f65b81f8e28536db958fa_E06E6362-8590-4F23-A149-373FA0DE0F30-p-500.webp",
+    types: ["DEX Aggregator"],
+  },
+  {
+    id: 15,
+    name: "Balancer",
+    desciption:
+      "Balancer is a community-owned and -governed decentralized exchange",
+    imgUrl:
+      "https://cdn.prod.website-files.com/65035c85edfbbecd98169f8d/6509e2c0d19e4527342b3c42_bnb-chain-binance-smart-chain-logo-p-500.png",
+    types: ["DEX"],
+  },
+  {
+    id: 16,
+    name: "Balancer",
+    desciption:
+      "Balancer is a community-owned and -governed decentralized exchange",
+    imgUrl:
+      "https://cdn.prod.website-files.com/65035c85edfbbecd98169f8d/667f65cf69cd32468cfd6c38_477B2EF5-8039-4077-B176-A9EDC2403FDD-p-500.png",
+    types: ["Defi Tool"],
+  },
+  {
+    id: 17,
+    name: "Balancer",
+    desciption:
+      "Balancer is a community-owned and -governed decentralized exchange",
+    imgUrl:
+      "https://cdn.prod.website-files.com/65035c85edfbbecd98169f8d/6509e1f85581fc8a0683e67d_polygon-matic-logo-p-500.png",
+    types: ["DEX Aggregator", "Defi Tool"],
+  },
+  {
+    id: 18,
+    name: "Balancer",
+    desciption:
+      "Balancer is a community-owned and -governed decentralized exchange",
+    imgUrl:
+      "https://cdn.prod.website-files.com/65035c85edfbbecd98169f8d/6668c0f3c5daccd9ff013254_360-avax.png",
+    types: ["DEX", "DEX Aggregator", "Defi Tool"],
+  },
 ];
 
 const Projects = () => {
@@ -130,10 +187,139 @@ const Projects = () => {
         <FontAwesomeIcon icon={faMagnifyingGlass} />
         <input type="text" placeholder="Search projects, chains, and more" />
       </div>
+      <div className="projects-category">
+        <div className="projects-category-relative">
+          <div className="projects-category-select">
+            <FontAwesomeIcon icon={faBorderAll} />
+            <span>Categories</span>
+            <FontAwesomeIcon icon={faChevronDown} />
+          </div>
+          <div className="projects-category-view"></div>
+        </div>
+        <div className="projects-category-relative">
+          <div className="projects-category-select">
+            <FontAwesomeIcon icon={faLink} />
+            <span>Chains</span>
+            <FontAwesomeIcon icon={faChevronDown} />
+          </div>
+          <div className="projects-category-chain-view">
+            <div className="projects-category-chain-view-item">
+              <div className="projects-category-chain-view-item-title">
+                Core Chains
+              </div>
+              <div className="projects-category-chain-view-item-list">
+                <div className="projects-category-chain-view-item-list-item">
+                  <input type="checkbox" />
+                  <img
+                    src="https://portal.arbitrum.io/images/ArbOneLogo.svg"
+                    alt="token"
+                  />
+                  <span>Arbitrum One</span>
+                </div>
+                <div className="projects-category-chain-view-item-list-item">
+                  <input type="checkbox" />
+                  <img
+                    src="https://portal.arbitrum.io/images/ArbNovaLogo.svg"
+                    alt="token"
+                  />
+                  <span>Arbitrum Nova</span>
+                </div>
+              </div>
+            </div>
+            <div className="projects-category-chain-view-item">
+              <div className="projects-category-chain-view-item-title">
+                Orbit Chains
+              </div>
+              <div className="projects-category-chain-view-item-list">
+                <div className="projects-category-chain-view-item-list-item">
+                  <input type="checkbox" />
+                  <img
+                    src="https://portal.arbitrum.io/_next/image?url=%2Fimages%2Forbitchains%2Fapechain-logo.webp&w=32&q=75"
+                    alt="token"
+                  />
+                  <span>ApeChain</span>
+                </div>
+                <div className="projects-category-chain-view-item-list-item">
+                  <input type="checkbox" />
+                  <img
+                    src="https://portal.arbitrum.io/_next/image?url=%2Fimages%2Forbitchains%2Fcheese-logo.webp&w=32&q=75"
+                    alt="token"
+                  />
+                  <span>CheeseChain</span>
+                </div>
+                <div className="projects-category-chain-view-item-list-item">
+                  <input type="checkbox" />
+                  <img
+                    src="https://portal.arbitrum.io/_next/image?url=%2Fimages%2Forbitchains%2Fdegen-logo.webp&w=32&q=75"
+                    alt="token"
+                  />
+                  <span>Degen Chain</span>
+                </div>
+                <div className="projects-category-chain-view-item-list-item">
+                  <input type="checkbox" />
+                  <img
+                    src="https://portal.arbitrum.io/_next/image?url=%2Fimages%2Forbitchains%2Febi-xyz-logo.webp&w=32&q=75"
+                    alt="token"
+                  />
+                  <span>ebi.xyz</span>
+                </div>
+                <div className="projects-category-chain-view-item-list-item">
+                  <input type="checkbox" />
+                  <img
+                    src="https://portal.arbitrum.io/_next/image?url=%2Fimages%2Forbitchains%2Finjective-logo.webp&w=32&q=75"
+                    alt="token"
+                  />
+                  <span>Injective EVM</span>
+                </div>
+                <div className="projects-category-chain-view-item-list-item">
+                  <input type="checkbox" />
+                  <img
+                    src="https://portal.arbitrum.io/_next/image?url=%2Fimages%2Forbitchains%2Fpop-apex-logo.webp&w=32&q=75"
+                    alt="token"
+                  />
+                  <span>Proof of Play Apex</span>
+                </div>
+                <div className="projects-category-chain-view-item-list-item">
+                  <input type="checkbox" />
+                  <img
+                    src="https://portal.arbitrum.io/_next/image?url=%2Fimages%2Forbitchains%2Freya-logo.webp&w=32&q=75"
+                    alt="token"
+                  />
+                  <span>Reya</span>
+                </div>
+                <div className="projects-category-chain-view-item-list-item">
+                  <input type="checkbox" />
+                  <img
+                    src="https://portal.arbitrum.io/_next/image?url=%2Fimages%2Forbitchains%2Fsanko-logo.webp&w=32&q=75"
+                    alt="token"
+                  />
+                  <span>Sanko</span>
+                </div>
+                <div className="projects-category-chain-view-item-list-item">
+                  <input type="checkbox" />
+                  <img
+                    src="https://portal.arbitrum.io/_next/image?url=%2Fimages%2Forbitchains%2Fxai-logo.webp&w=32&q=75"
+                    alt="token"
+                  />
+                  <span>Xai</span>
+                </div>
+                <div className="projects-category-chain-view-item-list-item">
+                  <input type="checkbox" />
+                  <img
+                    src="https://portal.arbitrum.io/_next/image?url=%2Fimages%2Forbitchains%2Fxchain-logo.webp&w=32&q=75"
+                    alt="token"
+                  />
+                  <span>XCHAIN</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
       <div className="projects-view">
         {data.map((item, key) => {
           return (
-            <a href="/project/detail" className="projects-view-item">
+            <a href="/project/detail" className="projects-view-item" key={key}>
               <button
                 type="button"
                 className="projects-view-item-bookmark"
